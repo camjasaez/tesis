4547bd0817cabb6c219a7eb83013e6ec5d921aea
Camilo Sáez Garrido <camjasaez@gmail.com>
2023-06-07 15:46:13 -0400

    Se mejora el main y modifica requirements

--- app/main.py
+++ app/main.py
@@ -1,15 +1,10 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
-from motor.motor_asyncio import AsyncIOMotorClient
 from config import settings
-from contextlib import asynccontextmanager
-
-# from api.routes.router import api_router
 from api.routes import api_router
 
 
-app = FastAPI()
 app = FastAPI(title=settings["APP_NAME"], debug=settings["DEBUG_MODE"])
 app.add_middleware(
     CORSMiddleware,
@@ -18,20 +13,9 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+
 app.include_router(api_router, prefix="/api")
-
-
-@asynccontextmanager
-async def db_client():
-    mongodb_client = AsyncIOMotorClient(settings["DB_URL"])
-    mongodb = mongodb_client[settings["DB_NAME"]]
-    print("DB connected")
-
-    try:
-        yield mongodb
-    finally:
-        mongodb_client.close()
-        print("DB disconnected")
 
 
 if __name__ == "__main__":
